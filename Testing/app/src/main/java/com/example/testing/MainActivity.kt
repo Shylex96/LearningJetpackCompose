9cf485e71563e0a00cc5766b747f3524f5282843
Shylex96 <estebanalbarran1996@gmail.com>
2024-04-10 12:47:53 +0200

    #03: Udemy practice

---
+++ Testing/app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.testing
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.testing.ui.theme.TestingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyTextField()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestingTheme {
+        MyTextField()
+    }
+}
+
+@Composable
+fun MyTextField() {
+    var myText by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = myText,
+        onValueChange = {
+            myText =
+                if (it.contains("a")) {
+                    it.replace("a", "")
+                } else {
+                    it
+                }
+        },
+        label = { Text(text = "Introduce tu nombre") })
+}
+
+
+// ---------------------------------------------------------------------------- \\
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo 1")
+        Text(text = "Esto es un ejemplo 2", color = Color.Blue)
+        Text(text = "Esto es un ejemplo 3", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo 4", fontWeight = FontWeight.ExtraLight)
+        Text(text = "Esto es un ejemplo 5", fontFamily = FontFamily.Cursive)
+        Text(
+            text = "Esto es un ejemplo 6",
+            textDecoration = TextDecoration.LineThrough
+        )
+        Text(
+            text = "Esto es un ejemplo 7",
+            textDecoration = TextDecoration.LineThrough
+        )
+        Text(
+            text = "Esto es un ejemplo 8",
+            textDecoration = TextDecoration.Underline
+        )
+        Text(
+            text = "Esto es un ejemplo 9", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.Underline, TextDecoration.LineThrough)
+                )
+            )
+        )
+        Text(text = "Esto es un ejemplo 10", fontSize = 30.sp)
+    }
+}
+
+
+// ---------------------------------------------------------------------------- \\
+
+@Composable
+fun MyStateExample() {
+    var contador by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { contador += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado ${contador} veces")
+    }
+}
