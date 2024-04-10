cc759646868dae3e90dd7747e315ca53ef2da1b2
Shylex96 <estebanalbarran1996@gmail.com>
2024-04-10 12:57:53 +0200

    #03: Outlined Textfield

--- Testing/app/src/main/java/com/example/testing/MainActivity.kt
+++ Testing/app/src/main/java/com/example/testing/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -23,6 +28,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testing.ui.theme.TestingTheme
 
@@ -36,7 +42,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyTextField()
+                    Column(){
+                        MyTextFieldOutlined()
+                    }
+
                 }
             }
         }
@@ -49,6 +58,26 @@ fun DefaultPreview() {
     TestingTheme {
         MyTextField()
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextFieldOutlined() {
+    var myText by remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        value = myText,
+        onValueChange = {
+            myText =
+                it
+        }, modifier = Modifier.padding(24.dp),
+        label = { Text(text = "Escribe tu nombre")},
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = Color.Blue,
+            unfocusedBorderColor = Color.Black,
+        )
+    )
 }
 
 @Composable
