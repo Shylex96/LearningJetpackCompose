9828a5131238986ffae2b1759544056b4e01a4b9
Shylex96 <estebanalbarran1996@gmail.com>
2024-04-16 12:37:32 +0200

    #07: Animations

---
+++ JetpackComposeAnimations/app/src/main/java/com/example/jetpackcomposeanimations/AnimationExamples.kt
@@ -0,0 +1,109 @@
+package com.example.jetpackcomposeanimations
+
+import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ColorAnimationSimple() {
+    Column() {
+        var firstColor by rememberSaveable {
+            mutableStateOf(false)
+        }
+        var realColor = if (firstColor) Color.Red else Color.Yellow
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor)
+            .clickable { firstColor = !firstColor })
+
+        Spacer(modifier = Modifier.size(200.dp))
+
+        var secondColor by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Red else Color.Yellow)
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor2)
+            .clickable { secondColor = !secondColor })
+    }
+}
+
+@Composable
+fun ColorAnimationAdvanced() {
+    var firstColor by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    var showBox by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    val realColor by animateColorAsState(
+        targetValue = if (firstColor) Color.Red else Color.Yellow,
+        animationSpec = tween(durationMillis = 2000),
+        finishedListener = { showBox = false }
+    )
+
+    if (showBox) {
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor)
+            .clickable { firstColor = !firstColor })
+    }
+}
+
+@Composable
+fun SizeAnimation() {
+
+    var smallSize by rememberSaveable { mutableStateOf(true) }
+    val size by animateDpAsState(targetValue = if (smallSize) 50.dp else 100.dp)
+
+    Column {
+        Box(modifier = Modifier
+            .size(size)
+            .background(Color.Cyan)
+            .clickable { smallSize = !smallSize })
+    }
+}
+
+@Composable
+fun SizeAnimationAdvanced() {
+
+    var smallSize by rememberSaveable { mutableStateOf(true) }
+    val size by animateDpAsState(
+        targetValue = if (smallSize) 50.dp else 100.dp,
+        animationSpec = tween(durationMillis = 500),
+        finishedListener = {
+            if (!smallSize) {
+
+            }
+        }
+    )
+
+    Column {
+        Box(modifier = Modifier
+            .size(size)
+            .background(Color.Cyan)
+            .clickable { smallSize = !smallSize })
+    }
+}
+
+@Composable
+fun VisibilityAnimation() {
+
+}
