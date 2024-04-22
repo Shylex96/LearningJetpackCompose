7ae6a733a74e5f06d931ed603055e7a6941a45f8
Shylex96 <estebanalbarran1996@gmail.com>
2024-04-22 10:09:20 +0200

    #09: Crossfade animation

--- JetpackComposeAnimations/app/src/main/java/com/example/jetpackcomposeanimations/AnimationExamples.kt
+++ JetpackComposeAnimations/app/src/main/java/com/example/jetpackcomposeanimations/AnimationExamples.kt
@@ -2,6 +2,7 @@ package com.example.jetpackcomposeanimations
 
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
@@ -12,7 +13,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
@@ -20,6 +24,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random.Default.nextInt
 
 @Composable
 fun ColorAnimationSimple() {
@@ -117,10 +122,54 @@ fun VisibilityAnimation() {
             Text(text = "Mostrar/Ocultar")
         }
         Spacer(modifier = Modifier.size(50.dp))
-        AnimatedVisibility (isVisible) {
-            Box(modifier = Modifier
-                .size(150.dp)
-                .background(Color.Red))
+        AnimatedVisibility(isVisible) {
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .background(Color.Red)
+            )
         }
     }
+}
+
+@Composable
+fun CrossfadeExampleAnimation() {
+
+    var myComponentType by remember {
+        mutableStateOf(ComponentType.Text)
+    }
+
+    Column(Modifier.fillMaxSize()) {
+        Button(onClick = { myComponentType = getComponentTypeRandom() }) {
+            Text(text = "Cambiar componente")
+        }
+
+        Crossfade(targetState = myComponentType) { myComponentType ->
+            when (myComponentType) {
+                ComponentType.Image -> Icon(Icons.Default.CheckCircle, contentDescription = "")
+                ComponentType.Text -> Text(text = "Soy un componente")
+                ComponentType.Box -> Box(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .background(Color.Red)
+                )
+                ComponentType.Error -> Text(text = "ERROR!")
+            }
+        }
+
+    }
+}
+
+fun getComponentTypeRandom(): ComponentType {
+
+    return when (nextInt(from = 0, until = 3)) {
+        0 -> ComponentType.Image
+        1 -> ComponentType.Text
+        2 -> ComponentType.Box
+        else -> ComponentType.Error
+    }
+}
+
+enum class ComponentType() {
+    Image, Text, Box, Error
 }
